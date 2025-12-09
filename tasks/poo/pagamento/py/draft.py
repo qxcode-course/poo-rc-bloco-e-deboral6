6653cb6c8514a3af3c96a02a8e14ff72a1def061
deboral6 <deboralemos997130537@gmail.com>
2025-12-09 14:41:31 +0000

    pagamento

--- tasks/poo/pagamento/py/draft.py
+++ tasks/poo/pagamento/py/draft.py
@@ -1,17 +1,22 @@
 from abc import ABC, abstractmethod
 
+class Metodo(ABC):
+    @abstractmethod
+    def realizar_pagamento(self, descricao, 1valor: float):
+        pass
+
 class Pagamento(ABC):
     def __init__(self, valor: float, descricao: str):
         self.valor = valor
         self.descricao = descricao
 
-    def resumo(self):
-        print(f"Pagamento de R$ {self.valor}: {self.descricao}")
-
     def validar_valor(self):
         if self.valor <= 0:
             raise ValueError(f"O valor deve ser maior que zero")
     
+    def resumo(self):
+        print(f"Pagamento de R$ {self.valor}: {self.descricao}")
+
     @abstractmethod
     def processar(self):
         pass
@@ -28,6 +33,7 @@ class CartaoCredito(Pagamento):
             raise Exception(f"Erro: Limite isuficiente no cartão {self.numero}")
         else:
             self.limite_disponivel -= self.valor
+            print(f"Pagamento no cartao, no valor de {self.valor}")
 
 class Pix(Pagamento):
     def __init__(self, valor, descriçao, chave, banco):
@@ -36,6 +42,16 @@ class Pix(Pagamento):
         self.banco = banco
 
     def processar(self):
+        print(f"Pagando pix produto {self.descricao} para {self.chave} do banco {self.banco} no valor de {self.valor}")
+
+class Boleto(Pagamento):
+    def __init__(self, valor, descricao, codigo_barras: int, vencimento):
+        super().__init__(valor, descriçao)
+        self.codigo_barras = codigo_barras
+        self.vencimento = vencimento
+
+    def processar(self):
+
         
 
 
